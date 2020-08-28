20c2728db53c50cf4ff4df12554d48a7399ce2dc
QQZAD <2298070398@qq.com>
2020-08-28 22:33:39 +0800

    shell

--- boost.sh
+++ boost.sh
@@ -3,7 +3,7 @@ echo "https://www.boost.org/"
 echo "https://www.boost.org/doc/libs/1_74_0/doc/html/thread.html"
 read -p "按回车键继续..."
 name=boost_1_74_0
-if [ ! -f "wincorefonts.zip" ]
+if [ ! -f "$name.tar.gz" ]
 then
     echo "在shell/中没有找到$name.tar.gz文件"
     exit
