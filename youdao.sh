c55f8c5ef98e90690a9ce229c497030589f7ac0b
QQZAD <2298070398@qq.com>
2020-09-30 17:13:32 +0800

    shell

--- youdao.sh
+++ youdao.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 option=1
 # 1 to install, 0 to uninstall
+# https://cidian.youdao.com/multi.html
 if [ -n "$1" ]; then
-	if [ $1 -eq "1" ]
-	then
-		option=1
-	else
-		option=0
-	fi
+    if [ $1 -eq "1" ]
+    then
+        option=1
+    else
+        option=0
+    fi
 fi
 if [ $option -eq 1 ]
 then
-	sudo echo "安装youdao..."
-	touch youdao.desktop
-	echo "#!/usr/bin/env xdg-open">> youdao.desktop
-	echo "[Desktop Entry]">> youdao.desktop
-	echo "Version=1.0">> youdao.desktop
-	echo "Terminal=false">> youdao.desktop
-	echo "Type=Link">> youdao.desktop
-	echo "Name=有道翻译网页版">> youdao.desktop
-	echo "URL=http://fanyi.youdao.com/">> youdao.desktop
-	echo "Icon=${HOME}/shell/icon/youdao.jpg">> youdao.desktop
-	sudo cp youdao.desktop ${HOME}/桌面
-	sudo mv youdao.desktop ${HOME}
-	sudo chown ${USER}: -R ${HOME}/youdao.desktop
-	sudo chown ${USER}: -R ${HOME}/桌面/youdao.desktop
+    sudo echo "安装youdao..."
+    name=youdao-dict_6.0.0-ubuntu-amd64.deb
+    if [ ! -f "./$name" ]
+    then
+        echo "正在从网络获取$name"
+        wget --no-check-certificate http://codown.youdao.com/cidian/linux/$name
+    else
+        echo "已经获取本地$name"
+    fi
+    sudo dpkg -i $name
+    sudo apt-get -y install -f
+    echo
+    echo -e "\e[32m如果安装过程出现错误请再次运行./youdao.sh 1\e[0m"
 else
-	sudo echo "卸载youdao..."
-	sudo rm -rf ${HOME}/youdao.desktop
-	sudo rm -rf ${HOME}/桌面/youdao.desktop
+    sudo echo "卸载youdao..."
+    sudo dpkg --purge youdao-dict
+    echo
+    echo -e "\e[32m如果卸载过程出现错误请运行./youdao.sh 1后再卸载\e[0m"
 fi
