09ea886e82d78088b6abdeed7946f77f84806f51
QQZAD <2298070398@qq.com>
2020-10-12 13:53:25 +0800

    shell

--- vmware_workstation.sh
+++ vmware_workstation.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 version=16
 read -p "按回车键继续..."
-sudo rm -rf ${HOME}/.vmware/VMWARE$version.der
+sudo apt-get install make gcc libaio1 libglib2.0-dev -y
+sudo rm -rf ${HOME}/.vmware/VMWARE$version.der ${HOME}/.vmware/VMWARE$version.priv
 sudo vmware-modconfig --console --install-all
+if [ ! -d ${HOME}/.vmware ]
+then
+    mkdir ${HOME}/.vmware
+fi
 cd ${HOME}/.vmware
 openssl req -new -x509 -newkey rsa:2048 -keyout VMWARE$version.priv -outform DER -out VMWARE$version.der -nodes -days 36500 -subj "/CN=VMWARE/"
 sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ./VMWARE$version.priv ./VMWARE$version.der $(modinfo -n vmmon)
