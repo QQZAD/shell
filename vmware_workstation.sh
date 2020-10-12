8cbb55f4dfe47c9fb3f48044e137422e7cef830b
QQZAD <2298070398@qq.com>
2020-10-12 11:03:41 +0800

    shell

--- vmware_workstation.sh
+++ vmware_workstation.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 version=16
-echo "正在检查VMware Workstation $version的配置"
-mokutil --test-key ${HOME}/.vmware/VMWARE$version.der
 read -p "按回车键继续..."
+sudo rm -rf ${HOME}/.vmware/VMWARE$version.der
 sudo vmware-modconfig --console --install-all
 cd ${HOME}/.vmware
 openssl req -new -x509 -newkey rsa:2048 -keyout VMWARE$version.priv -outform DER -out VMWARE$version.der -nodes -days 36500 -subj "/CN=VMWARE/"
@@ -12,4 +11,5 @@ tail $(modinfo -n vmmon) | grep "Module signature appended"
 echo "密码123456"
 echo "输入密码后系统将重启"
 sudo mokutil --import VMWARE$version.der
+mokutil --test-key VMWARE$version.der
 reboot
