a56b60c3b9f6cc2812ced40a22086abd78d1bdd5
QQZAD <2298070398@qq.com>
2020-12-25 18:04:32 +0800

    shell

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "服务器安装Ubuntu18会出现错误"
 echo "服务器断电维护"
 echo "sudo ifconfig enp0s31f6 up"
 echo "sudo dhclient -r -v enp0s31f6 && rm /var/lib/dhcp/dhclient.* ;sudo dhclient -v enp0s31f6"
