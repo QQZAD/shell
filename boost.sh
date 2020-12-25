46e6887403e51d64f8a41ffe803b61386b39d9d5
QQZAD <2298070398@qq.com>
2020-12-25 21:42:58 +0800

    shell

--- boost.sh
+++ boost.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "https://www.boost.org/"
-echo "https://www.boost.org/doc/libs/1_74_0/doc/html/thread.html"
+echo "https://www.boost.org/doc/libs/1_75_0/doc/html/thread.html"
 read -p "按回车键继续..."
-name=boost_1_74_0
+name=boost_1_75_0
 if [ ! -f "$name.tar.gz" ]
 then
     echo "在shell/中没有找到$name.tar.gz文件"
