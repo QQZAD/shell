1045cc5df5ba86d67a87e6b454debf57fecd60cd
QQZAD <2298070398@qq.com>
2021-01-13 15:58:53 +0800

    shell

---
+++ gwy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "我校入围国家“双一流”建设高校及建设学科名单"
+echo "https://www.cug.edu.cn/info/10506/88940.htm"
+echo "“双一流”建设高校名单"
+echo "http://www.moe.gov.cn/s78/A22/A22_ztzl/ztzl_tjsylpt/sylpt_jsgx/201712/t20171206_320667.html"
+echo "2020年湖北省选调生招录公告"
+echo "http://www.hbsrsksy.cn/hbksy/004/004001/004001004/004001004001/20191213/b7b14083-7c9f-4142-acea-2040d2f89e31.html"
+echo "2021年湖北省选调生招录公告"
+echo "http://www.hbsrsksy.cn/hbksy/004/004001/004001004/004001004001/20210108/ccba0adf-9197-42e0-99d2-dfb4084f2346.html"
